2ed6971a79f9637cb42176a8fa174313c096244c
JOrefice <John.Orefice@emerson.com>
2018-09-10 14:12:41 -0600

    Added maven print statement

--- src/main/java/ClassA.java
+++ src/main/java/ClassA.java
@@ -3,6 +3,7 @@ public class ClassA {
     public static void main(String[] args)
     {
         System.out.println(helloWorld());
+        System.out.println("Using Maven now");
     }
 
     public static String helloWorld()
